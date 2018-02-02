ba92cf666d411d4c6655eaaa1a45bb0f09e98c49
Travis CI <travis@travis-ci.org>
2018-02-02 19:40:44 +0800

    add ci

--- gen.js
+++ gen.js
@@ -2,23 +2,12 @@
 const {execSync} = require('child_process');
 const {writeFileSync} = require('fs');
 const {resolve} = require('path');
-const ghPages = require('gh-pages');
 
 // 生成网页
 console.log('开始生成网页静态文件');
-execSync(`gitbook build`);
+execSync(`node ${resolve(__dirname, 'node_modules/.bin/gitbook')} build`);
 console.log('生成网页静态文件成功');
 
 // 自定义域名
 writeFileSync(resolve('_book', 'CNAME'), 'go.wuhaolin.cn');
-
-// 发布
-console.log('开始发布');
-ghPages.publish('_book', (err) => {
-    if (err) {
-        console.error('发布失败', err);
-    }
-    console.info('发布成功');
-});
-
-
+console.log('生成静态网站完毕');
